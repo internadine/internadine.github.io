d4c9b71197392a85f258f5de6e570503ddd17da8
Nadine Wischmeier <Nadine.Wischmeier@outlook.com>
2017-07-20 14:44:33 +0200

    add imprint and linked pages

--- assets/js/wischmeier.js
+++ assets/js/wischmeier.js
@@ -1,1 +1,11 @@
 // JavaScript source code
+
+var gaProperty = 'UA-102848389-1';
+var disableStr = 'ga-disable-' + gaProperty;
+if (document.cookie.indexOf(disableStr + '=true') > -1) {
+        window[disableStr] = true;
+    }
+function gaOptout() {
+        document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+    window[disableStr] = true;
+}
